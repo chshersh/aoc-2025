4bc25321b65d12d7d5100b3215a02946d285be65
Dmitrii Kovanikov <kovanikov@gmail.com>
2025-12-05 19:57:28 +0000

    Solve Day 3 (Part 2)

--- day03/main.cpp
+++ day03/main.cpp
@@ -22,10 +22,11 @@ std::string read_file(const std::filesystem::path& path) {
     return sv | std::views::split('\n');
 }
 
-int max_joltage(std::string_view bank) {
-    int max = 0;
 
-    constexpr auto to_digit = [](char c) -> int { return c - '0'; };
+int to_digit(char c) { return c - '0'; }
+
+int max_joltage1(std::string_view bank) {
+    int max = 0;
 
     for (size_t i = 0; i < bank.size() - 1; ++i) {
         for (size_t j = i + 1; j < bank.size(); ++j) {
@@ -37,12 +38,28 @@ int max_joltage(std::string_view bank) {
     return max;
 }
 
-int solve_part1(std::string_view contents) {
-    int sum = 0;
+long long max_joltage2(std::string_view bank, size_t start, size_t len, long long acc) {
+    if (len == 0) return acc;
+
+    size_t max_digit_pos = 0;
+    int max_digit = 0;
+
+    for (size_t i = start; i < bank.size() - len + 1; ++i) {
+        int digit = to_digit(bank[i]);
+        if (digit > max_digit) {
+            max_digit_pos = i;
+            max_digit = digit;
+        }
+    }
+
+    return max_joltage2(bank, max_digit_pos + 1, len - 1, acc * 10 + max_digit);
+}
+
+long long solve(std::string_view contents) {
+    long long sum = 0;
 
     for (auto line : lines(contents)) {
-        sum += max_joltage(std::string_view{line});
-
+        sum += max_joltage2(std::string_view{line}, 0, 12, 0);;
     }
 
     return sum;
@@ -57,5 +74,5 @@ int main(int argc, char* argv[]) {
     std::filesystem::path file_path = argv[1];
     auto file_contents = read_file(file_path);
 
-    std::println("{}", solve_part1(file_contents));
+    std::println("{}", solve(file_contents));
 }
