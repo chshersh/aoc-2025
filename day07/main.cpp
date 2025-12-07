86028abb0f48d558f78997700265e348366927b9
Dmitrii Kovanikov <kovanikov@gmail.com>
2025-12-07 18:13:46 +0000

    Solve Day 7 (Part 2)

--- day07/main.cpp
+++ day07/main.cpp
@@ -1,40 +1,37 @@
 // std includes
+#include <algorithm>
 #include <vector>
 
 // local
 #include <aoc_common.hpp>
 
-int solve(std::string_view contents) {
+long long solve(std::string_view contents) {
     const std::vector<std::string_view> diagram =
         aoc::lines(contents) | std::ranges::to<std::vector>();
 
     size_t rows = diagram.size();
     size_t cols = diagram[0].size();
-    std::vector<std::vector<int>> rays(rows, std::vector<int>(cols, 0));
+    std::vector<std::vector<long long>> paths(rows, std::vector<long long>(cols, 0));
 
     for (size_t col = 0; col < cols; ++col) {
-        if (diagram[0][col] == 'S') rays[0][col] = 1;
+        if (diagram[0][col] == 'S') paths[0][col] = 1;
     }
-
-    int splits = 0;
 
     for (size_t row = 1; row < rows; ++row) {
         for (size_t col = 0; col < cols; ++col) {
-            if (rays[row - 1][col] == 0) continue;
             switch (diagram[row][col]) {
                 case '.':
-                    rays[row][col] = 1;
+                    paths[row][col] += paths[row - 1][col];
                     break;
                 case '^':
-                    rays[row][col - 1] = 1;
-                    rays[row][col + 1] = 1;
-                    splits++;
+                    paths[row][col - 1] += paths[row - 1][col];
+                    paths[row][col + 1] += paths[row - 1][col];
                     break;
             }
         }
     }
 
-    return splits;
+    return std::ranges::fold_left(paths.back(), 0, std::plus{});
 }
 
 int main(int argc, char* argv[]) {
